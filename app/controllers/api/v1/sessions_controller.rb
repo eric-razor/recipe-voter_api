970d974cab10480025a94b31e0e71cb6aa1e92ed
eric <eric.razorjr@gmail.com>
2020-08-18 08:31:09 -0400

    Fix user serialization

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email])
 
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
