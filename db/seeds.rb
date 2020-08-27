333292e5b60da01e82984ba0b098c479a7c88064
eric <eric.razorjr@gmail.com>
2020-08-27 13:49:29 -0400

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-eric = User.create!(name: "erika", email:"erica@email.com",password:"password")
+eric = User.create!(name: "eric", email:"eric@email.com",password:"password")
 pancakes = Card.create!(recipe_name:"homemade pancakes", recipe_steps:"1. put stuff in bowl 2. put more stuff 3. cook 4. enjoy", recipe_ingredients:"self rising flour, eggs, sugar, milk", user_id: 1)
 pfr = Card.create!(recipe_name: "Fried rice", recipe_steps:"1.Cook rice 2. put stuff in it 3. enjoy", recipe_ingredients:"rice, veggies, stuff u wanna put in it idk", user_id: 1)
 
